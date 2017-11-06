a0fe3e63fc768356c4df215b44ad51d75e15877f
Thomas Strobel <4ZKTUB6TEP74PYJOPWIR013S2AV29YUBW5F9ZH2F4D5UMJUJ6S@hash.domains>
2017-11-06 12:13:42 +0100

    Prepare switch-to-configuration.pl for NixUP
    switch-to-configuration.sh is generalized to be equally usable
    for NixOS and for NixUP.
    The changes do not affect the behaviour for NixOS.

--- nixos/modules/system/activation/top-level.nix
+++ nixos/modules/system/activation/top-level.nix
@@ -128,6 +128,7 @@ let
 
       # Needed by switch-to-configuration.
       perl = "${pkgs.perl}/bin/perl -I${pkgs.perlPackages.FileSlurp}/lib/perl5/site_perl";
+      flavour = "nixos";
   } else throw "\nFailed assertions:\n${concatStringsSep "\n" (map (x: "- ${x}") failed)}");
 
   # Replace runtime dependencies
